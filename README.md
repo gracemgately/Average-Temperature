ad417a01c31b3ad634172a2b51ed224285ecae37
Grace Gately <gracemgately@gmail.com>
2018-03-08 13:59:21 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+_A simple csv file parser to find the average temperature in North America for the day of March 8, 2018._  
+***Link: https://www7.ncdc.noaa.gov/CDO/cdoselect.cmd
---
+++ index.js
@@ -0,0 +1,35 @@
+const parse = require('csv-parse');
+const fs = require('fs');
+const transform = require('stream-transform');
+
+const fileToRead = fs.createReadStream('NNDC__03_08_18.txt'); 
+
+const parser = parse({delimiter: ','});
+parser.on('error', (err) => console.error(err));
+
+class parseAndFindAverageTemp{
+    constructor(){
+        this.numRecords = 0;
+        this.total = 0;
+    }
+    addToRecords(record){
+        if (!isNaN(Number(record))) {
+            this.total += Number(record);
+            this.numRecords++; 
+        }
+    }
+    getAverageTemp(){
+        return this.total / this.numRecords;
+    }
+}
+
+const recordsAndAverageTemp = new parseAndFindAverageTemp();
+
+const transformer = transform((record) => recordsAndAverageTemp.addToRecords(record[3]), {parallel: 10});
+transformer.on('error', (err) => console.error(err));
+
+const stream = fileToRead.pipe(parser).pipe(transformer);
+stream.on('finish', () => {
+    console.log(recordsAndAverageTemp.getAverageTemp());
+})
+
